3b1a82a7eceba5be9220505282018bc5a45bccce
sntnmjones <sntnmjones@gmail.com>
2023-02-22 20:27:41 -0800

    Updated secret key
    Updated secret key and add env loading

--- rental_app/settings.py
+++ rental_app/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$9nm^&s_94!7g(nn8k1vw9vw6kc6ke!17mm(%$bmgb&t*+2r9e'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+print('Loading SECRET_KEY from .env')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
