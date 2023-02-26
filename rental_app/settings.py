8ad15432ac91b70546e8a8c8781a7fb05d4b8097
sntnmjones <sntnmjones@gmail.com>
2023-02-26 06:29:42 -0800

    Onboarded to supabase
    https://github.com/sntnmjones/RentalApp/issues/10

--- rental_app/settings.py
+++ rental_app/settings.py
@@ -24,21 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
-if SECRET_KEY != "":
-    print("Loaded SECRET_KEY from .env")
-else:
-    print("No SECRET_KEY found in .env. Exiting...")
-    exit()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv("DEBUG"))
-print(f"Debug = {DEBUG}")
+DEBUG = True # get_env_var("DEBUG")
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -46,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -81,18 +74,21 @@ WSGI_APPLICATION = "rental_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+POSTGRES_PASSWORD = str(os.getenv("POSTGRES_PASSWORD"))
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": "db.zmcavbxpvdadyszaipaq.supabase.co",
+        "PORT": "5432",
     }
 }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
