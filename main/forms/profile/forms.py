7ca899bb3c6e17a56ad069cc5a195a9fc0fe5dea
Troy Jones <sntnmjones@gmail.com>
2023-03-26 15:29:32 -0700

    Revert "Change user experience to email login"
    This reverts commit 07a200c1d7c19130be8df581ae9912adea2e8a20.

--- main/forms/profile/forms.py
+++ main/forms/profile/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -24,18 +23,6 @@ class NewUserForm(UserCreationForm):
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
-        # username is primary key. We're making this the email so only one user
-        # per email
-        user.username = self.cleaned_data["email"]
-        user.first_name = self.cleaned_data["username"]
-
         if commit:
             user.save()
         return user
-
-
-class EmailAuthenticationForm(AuthenticationForm):
-    # TODO: Add validation for email
-    # https://github.com/sntnmjones/RentalApp/issues/16
-    None
-    
--- main/views/profile/view.py
+++ main/views/profile/view.py
@@ -1,14 +1,12 @@
 import logging
 from django.shortcuts import render
 from django.shortcuts import render, redirect
-from main.forms.profile.forms import NewUserForm, EmailAuthenticationForm
+from main.forms.profile.forms import NewUserForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 
 logger = logging.getLogger()
-user_profile_template: str = "main/templates/profile/user_profile.html"
-login_form_template: str = "main/templates/profile/login_form.html"
-new_user_form_template: str = "main/templates/profile/new_user_form.html"
+
 
 def register(request):
     logger.info(request.META)
@@ -21,13 +19,13 @@ def register(request):
         logger.warning("Could not register user. %s", form.errors.as_json)
         return render(
             request,
-            template_name=new_user_form_template,
+            template_name="main/templates/profile/new_user_form.html",
             context={"errors": form.errors, "register_form": form},
         )
     form = NewUserForm()
     return render(
         request,
-        template_name=new_user_form_template,
+        template_name="main/templates/profile/new_user_form.html",
         context={"register_form": form},
     )
 
@@ -35,46 +33,40 @@ def register(request):
 def user_profile(request):
     return render(
         request,
-        template_name=user_profile_template,
+        template_name="main/templates/profile/user_profile.html",
         context={},
     )
 
 
 def user_login(request):
     if request.method == "POST":
-        form = EmailAuthenticationForm(request, data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            email_address = form.cleaned_data.get("username")
+            username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-
-            user = authenticate(username=email_address, password=password)
+            user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return render(
-                    request,
-                    template_name=user_profile_template,
-                    context={"user": user.first_name},
-                )
+                return user_profile(request)
             else:
                 return render(
                     request,
-                    template_name=login_form_template,
+                    template_name="main/templates/profile/login_form.html",
                     context={"login_form": form, "errors": form.errors},
                 )
         else:
             return render(
                 request,
-                template_name=login_form_template,
+                template_name="main/templates/profile/login_form.html",
                 context={"login_form": form, "errors": form.errors},
             )
-    form = EmailAuthenticationForm()
+    form = AuthenticationForm()
     return render(
         request=request,
-        template_name=login_form_template,
+        template_name="main/templates/profile/login_form.html",
         context={"login_form": form},
     )
 
-
 def user_logout(request):
-    logout(request)
-    return redirect("/")
+	logout(request)
+	return redirect("/")
