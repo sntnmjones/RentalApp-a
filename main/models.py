cb470594d66a4b1b5d9473a6173a50b5fe2684d0
Troy Jones <sntnmjones@gmail.com>
2023-04-01 07:50:28 -0700

    Changed model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 
 class Address(models.Model):
@@ -12,33 +13,50 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.street_number} {self.street_name}, {self.city}, {self.state} {self.zip_code}"
 
+
 class Property(models.Model):
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
-    property_review = models.ManyToManyField('Review')
-    rating = models.FloatField()
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.address} - {self.rating}"
+        return f"{self.address}"
+
 
 class Review(models.Model):
     RATING_CHOICES = [
-        (0, '0'),
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
+        (0, "0"),
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
     ]
 
-    reviewed_property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=RATING_CHOICES)
-    starting_rent = models.IntegerField(default=0)
-    ending_rent = models.IntegerField(default=0)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    title = models.CharField(default='Review', max_length=100)
     comment = models.CharField(max_length=3000)
-    pub_date = models.DateTimeField('date published')
-    rented_duration = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    starting_rent = models.IntegerField(
+        blank=True, null=True, validators=[MaxValueValidator(999999)]
+    )
+    starting_rent_month_year = models.DateField(blank=True, null=True)
+    ending_rent = models.IntegerField(
+        blank=True, null=True, validators=[MaxValueValidator(999999)]
+    )
+    ending_rent_month_year = models.DateField(blank=True, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    rented_duration_in_months = models.IntegerField(blank=True, null=True, validators=[MaxValueValidator(9999)])
+
+    class Meta:
+        unique_together = (
+            "property",
+            "user",
+            "starting_rent_month_year",
+            "ending_rent_month_year",
+        )
 
     def __str__(self):
-        return (f"user: {self.user} - rating: {self.rating} - rented_duration: {self.rented_duration}"
-        f"{self.comment}")
+        return (
+            f"user: {self.user} - rating: {self.rating}"
+            f"{self.comment}"
+        )
